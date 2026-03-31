3e55f56e900debe6cdcea0bb6aee647d5a96dc81
iulicovete-ux <iulicovete@gmail.com>
2026-03-31 22:51:19 +0300

    Create permissions.js

---
+++ src/config/permissions.js
@@ -0,0 +1,34 @@
+const ROLE_ADMIN_ID = process.env.ROLE_ADMIN_ID;
+const ROLE_MEMBRU_MARE_ID = process.env.ROLE_MEMBRU_MARE_ID;
+const ROLE_MEMBRU_MIC_ID = process.env.ROLE_MEMBRU_MIC_ID;
+
+const MANAGE_POINTS_ROLE_IDS = [
+  ROLE_ADMIN_ID,
+  ROLE_MEMBRU_MARE_ID,
+  ROLE_MEMBRU_MIC_ID,
+].filter(Boolean);
+
+const RESET_POINTS_ROLE_IDS = [
+  ROLE_ADMIN_ID,
+].filter(Boolean);
+
+function memberHasAnyRole(member, roleIds) {
+  if (!member || !member.roles || !member.roles.cache) {
+    return false;
+  }
+
+  return roleIds.some((roleId) => member.roles.cache.has(roleId));
+}
+
+function canManagePoints(member) {
+  return memberHasAnyRole(member, MANAGE_POINTS_ROLE_IDS);
+}
+
+function canResetPoints(member) {
+  return memberHasAnyRole(member, RESET_POINTS_ROLE_IDS);
+}
+
+module.exports = {
+  canManagePoints,
+  canResetPoints,
+};
