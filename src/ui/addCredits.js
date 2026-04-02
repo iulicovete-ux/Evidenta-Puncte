fc21c41100b9ba6b443a77ecc926386b13d7bdfb
iulicovete-ux <iulicovete@gmail.com>
2026-04-03 01:55:35 +0300

    Update addCredits.js

--- src/ui/addCredits.js
+++ src/ui/addCredits.js
@@ -1,1 +1,38 @@
+const {
+  ActionRowBuilder,
+  UserSelectMenuBuilder,
+  StringSelectMenuBuilder,
+} = require("discord.js");
+const { getAllCreditActivities } = require("../config/creditActivities");
 
+function buildAddCreditsUserSelectRow() {
+  const userSelect = new UserSelectMenuBuilder()
+    .setCustomId("add_credits_user_select")
+    .setPlaceholder("Alege membrul")
+    .setMinValues(1)
+    .setMaxValues(1);
+
+  return new ActionRowBuilder().addComponents(userSelect);
+}
+
+function buildCreditActivitySelectRow(targetUserId) {
+  const activities = getAllCreditActivities();
+
+  const options = Object.entries(activities).map(([key, value]) => ({
+    label: value.label,
+    value: key,
+    description: `${value.credits} credit`,
+  }));
+
+  const selectMenu = new StringSelectMenuBuilder()
+    .setCustomId(`add_credits_activity:${targetUserId}`)
+    .setPlaceholder("Alege activitatea")
+    .addOptions(options);
+
+  return new ActionRowBuilder().addComponents(selectMenu);
+}
+
+module.exports = {
+  buildAddCreditsUserSelectRow,
+  buildCreditActivitySelectRow,
+};
