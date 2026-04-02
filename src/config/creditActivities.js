dda332698391c54eebbd153377d8322b0f22b0a6
iulicovete-ux <iulicovete@gmail.com>
2026-04-03 01:54:13 +0300

    Update creditActivities.js

--- src/config/creditActivities.js
+++ src/config/creditActivities.js
@@ -1,1 +1,28 @@
+const CREDIT_ACTIVITIES = {
+  jaf: {
+    label: "Jaf",
+    credits: 1,
+  },
+  rapire: {
+    label: "Răpire",
+    credits: 1,
+  },
+  razie: {
+    label: "Razie",
+    credits: 1,
+  },
+};
 
+function getCreditActivity(key) {
+  return CREDIT_ACTIVITIES[key] || null;
+}
+
+function getAllCreditActivities() {
+  return CREDIT_ACTIVITIES;
+}
+
+module.exports = {
+  CREDIT_ACTIVITIES,
+  getCreditActivity,
+  getAllCreditActivities,
+};
